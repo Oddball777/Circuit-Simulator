d578c9ea07b9f8bcb19ee5cc39906d31773300fe
Gustave Coulombe <magikgus@gmail.com>
2023-03-28 18:32:56 -0400

    Modified to only pass previous component/wire

--- circuit_simulator/basic_circuit_elements.py
+++ circuit_simulator/basic_circuit_elements.py
@@ -2,32 +2,61 @@ from abc import ABC
 from dataclasses import dataclass
 
 
+class Component(ABC):
+    @property
+    def isOn(self):
+        raise NotImplementedError
+
+
 @dataclass
 class Wire:
-    isOn: bool = False
+    inputs: list[Component]
+
+    @property
+    def isOn(self):
+        return any([component.isOn for component in self.inputs])
 
 
-class Source:
-    def __init__(self, outputs: list[Wire]) -> None:
-        self.outputs = outputs
-        self.isOn = False
+@dataclass
+class Source(Component):
+    active: bool = False
+
+    @property
+    def isOn(self):
+        return self.active
 
     def toggle(self):
-        self.isOn = not self.isOn
-        for wire in self.outputs:
-            wire.isOn = not wire.isOn
+        self.active = not self.active
 
     def toggle_on(self):
-        if not self.isOn:
+        if not self.active:
             self.toggle()
 
     def toggle_off(self):
-        if self.isOn:
+        if self.active:
             self.toggle()
 
 
 @dataclass
-class Light:
+class Light(Component):
+    inputs: list[Wire]
+
+    @property
+    def isOn(self) -> bool:
+        return any([wire.isOn for wire in self.inputs])
+
+
+@dataclass
+class AndGate(Component):
+    inputs: list[Wire]
+
+    @property
+    def isOn(self) -> bool:
+        return all([wire.isOn for wire in self.inputs])
+
+
+@dataclass
+class OrGate(Component):
     inputs: list[Wire]
 
     @property
--- tests/test_basic_circuit_elements.py
+++ tests/test_basic_circuit_elements.py
@@ -1,48 +1,54 @@
 import unittest
 
-from circuit_simulator.basic_circuit_elements import Light, Source, Wire
+from circuit_simulator.basic_circuit_elements import AndGate, Light, Source, Wire
 
 
 class TestWire(unittest.TestCase):
     def test_init(self):
-        wire = Wire()
+        source = Source()
+        wire = Wire([source])
         self.assertFalse(wire.isOn)
 
 
 class TestSource(unittest.TestCase):
-    def test_init(self):
-        wire = Wire()
-        source = Source([wire])
+    def test_init_off(self):
+        source = Source()
         self.assertFalse(source.isOn)
 
-    def test_toggle(self):
-        wire = Wire()
-        source = Source([wire])
+    def test_toggle_off_to_on(self):
+        source = Source()
+        wire = Wire([source])
         source.toggle()
         self.assertTrue(wire.isOn)
 
+    def test_toggle_on_to_off(self):
+        source = Source(active=True)
+        wire = Wire([source])
+        source.toggle()
+        self.assertFalse(wire.isOn)
+
     def test_toggle_on_if_off(self):
-        wire = Wire()
-        source = Source([wire])
+        source = Source()
+        wire = Wire([source])
         source.toggle_on()
         self.assertTrue(wire.isOn)
 
     def test_toggle_on_if_on(self):
-        wire = Wire()
-        source = Source([wire])
+        source = Source()
+        wire = Wire([source])
         source.toggle()
         source.toggle_on()
         self.assertTrue(wire.isOn)
 
     def test_toggle_off_if_off(self):
-        wire = Wire()
-        source = Source([wire])
+        source = Source()
+        wire = Wire([source])
         source.toggle_off()
         self.assertFalse(wire.isOn)
 
     def test_toggle_off_if_on(self):
-        wire = Wire()
-        source = Source([wire])
+        source = Source()
+        wire = Wire([source])
         source.toggle()
         source.toggle_off()
         self.assertFalse(wire.isOn)
@@ -50,25 +56,57 @@ class TestSource(unittest.TestCase):
 
 class TestLight(unittest.TestCase):
     def test_init(self):
-        wire = Wire()
-        source = Source([wire])
+        source = Source()
+        wire = Wire([source])
         light = Light([wire])
         self.assertFalse(light.isOn)
 
     def test_isOn_false(self):
-        wire1 = Wire()
-        wire2 = Wire()
-        source1 = Source([wire1])
-        source2 = Source([wire2])
+        source1 = Source()
+        source2 = Source()
+        wire1 = Wire([source1])
+        wire2 = Wire([source2])
         light = Light([wire1, wire2])
         self.assertFalse(light.isOn)
 
     def test_isOn_true(self):
-        wire1 = Wire()
-        wire2 = Wire()
-        source1 = Source([wire1])
-        source2 = Source([wire2])
+        source1 = Source()
+        source2 = Source()
+        wire1 = Wire([source1])
+        wire2 = Wire([source2])
         light = Light([wire1, wire2])
         source1.toggle()
         source2.toggle()
         self.assertTrue(light.isOn)
+
+
+class TestAndGate(unittest.TestCase):
+    def test_init_all_on(self):
+        source1 = Source(active=True)
+        source2 = Source(active=True)
+        source3 = Source(active=True)
+        wire1 = Wire([source1])
+        wire2 = Wire([source2])
+        wire3 = Wire([source3])
+        and_gate = AndGate([wire1, wire2, wire3])
+        self.assertTrue(and_gate.isOn)
+
+    def test_init_one_off(self):
+        source1 = Source(active=False)
+        source2 = Source(active=True)
+        source3 = Source(active=True)
+        wire1 = Wire([source1])
+        wire2 = Wire([source2])
+        wire3 = Wire([source3])
+        and_gate = AndGate([wire1, wire2, wire3])
+        self.assertFalse(and_gate.isOn)
+
+    def test_init_all_off(self):
+        source1 = Source(active=False)
+        source2 = Source(active=False)
+        source3 = Source(active=False)
+        wire1 = Wire([source1])
+        wire2 = Wire([source2])
+        wire3 = Wire([source3])
+        and_gate = AndGate([wire1, wire2, wire3])
+        self.assertFalse(and_gate.isOn)
